d791abebe3d17b52fcd37edc3c2b43397a7a8fbd
Kotaro Inoue <kinoue@yama.info.waseda.ac.jp>
2019-11-01 18:46:46 +0900

    Add clang-format

--- src/client.cpp
+++ src/client.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <fstream>
-#include <NTL/ZZ.h>
 #include <NTL/BasicThreadPool.h>
-#include "FHE.h"
-#include "timing.h"
-#include "EncryptedArray.h"
+#include <NTL/ZZ.h>
 #include <NTL/lzz_pXFactoring.h>
-#include <cassert>
-#include <cstdio>
-#include <ctime>
-#include <sstream>
-#include <iomanip>
-#include <thread>
-#include <chrono>
-#include <random>
 #include <algorithm>
 #include <boost/algorithm/string.hpp>
 #include <boost/asio.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <cassert>
+#include <chrono>
+#include <cstdio>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <thread>
+#include "EncryptedArray.h"
+#include "FHE.h"
+#include "timing.h"
 using namespace std;
 
 NTL_CLIENT
@@ -26,7 +26,8 @@ NTL_CLIENT
 //
 // Usage: client [IP/DOMAIN] [PORT]
 
-string return_current_time_and_date(){
+string return_current_time_and_date()
+{
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
@@ -35,150 +36,173 @@ string return_current_time_and_date(){
     return ss.str();
 }
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-  ifstream fdbbasics("../settings/dbbasics.bin", std::ios::binary);
-  bool dbstatus;
-  assert(fdbbasics>>dbstatus);
-  assert(dbstatus);
-  int numRec, totMed, totSide;
-  assert(fdbbasics>>numRec);
-  assert(fdbbasics>>totMed);
-  assert(fdbbasics>>totSide);
-  fdbbasics.close();
+    ifstream fdbbasics("../settings/dbbasics.bin", std::ios::binary);
+    bool dbstatus;
+    assert(fdbbasics >> dbstatus);
+    assert(dbstatus);
+    int numRec, totMed, totSide;
+    assert(fdbbasics >> numRec);
+    assert(fdbbasics >> totMed);
+    assert(fdbbasics >> totSide);
+    fdbbasics.close();
 
-  ifstream fctxt("../settings/context.bin", std::ios::binary);
-  unsigned long m, p, r;
-  std::vector<long> gens, ords;
-  readContextBase(fctxt, m, p, r, gens, ords);
-  FHEcontext context(m, p, r, gens, ords);
-  assert(fctxt>>context);
-  fctxt.close();
-  //read context from context.bin
+    ifstream fctxt("../settings/context.bin", std::ios::binary);
+    unsigned long m, p, r;
+    std::vector<long> gens, ords;
+    readContextBase(fctxt, m, p, r, gens, ords);
+    FHEcontext context(m, p, r, gens, ords);
+    assert(fctxt >> context);
+    fctxt.close();
+    // read context from context.bin
 
-  ZZX G=context.alMod.getFactorsOverZZ()[0];
-  EncryptedArray ea(context, G);
-  //generate ea from context
+    ZZX G = context.alMod.getFactorsOverZZ()[0];
+    EncryptedArray ea(context, G);
+    // generate ea from context
 
-  FHESecKey secretKey(context);
-  ifstream fskey("../settings/sk.bin", std::ios::binary);
-  assert(fskey>>secretKey);
-  fskey.close();
-  //read secretKey from sk.bin
+    FHESecKey secretKey(context);
+    ifstream fskey("../settings/sk.bin", std::ios::binary);
+    assert(fskey >> secretKey);
+    fskey.close();
+    // read secretKey from sk.bin
 
-  FHEPubKey publicKey(context);
-  ifstream fpkey("../settings/pk.bin", std::ios::binary);
-  assert(fpkey>>publicKey);
-  fpkey.close();
-  //read publicKey from pk.bin
+    FHEPubKey publicKey(context);
+    ifstream fpkey("../settings/pk.bin", std::ios::binary);
+    assert(fpkey >> publicKey);
+    fpkey.close();
+    // read publicKey from pk.bin
 
-  assert(argc==7);
-  string ip_address(argv[1]);
-  string port(argv[2]);
+    assert(argc == 7);
+    string ip_address(argv[1]);
+    string port(argv[2]);
 
-  using boost::asio::ip::tcp;
-  tcp::iostream connect(ip_address.c_str(), port.c_str());
-  if (!connect){
-    cerr<<return_current_time_and_date()<<" Can not connect: "<<connect.error().message()<<endl;
-    return 1;
-  }
+    using boost::asio::ip::tcp;
+    tcp::iostream connect(ip_address.c_str(), port.c_str());
+    if (!connect)
+    {
+        cerr << return_current_time_and_date()
+             << " Can not connect: " << connect.error().message() << endl;
+        return 1;
+    }
 
-  cout<<return_current_time_and_date()<<" Successfully created connection with "<<ip_address<<":"<<port<<endl;
-  int age = stoi(argv[3]);
-  assert(age>=0&&age<=122);
-  char gender = *argv[4];
-  assert(gender=='m'||gender=='f');
-  int mask=age+(gender=='m'?1:0)*128+5;
-  Ctxt enc_mask(publicKey);
-  publicKey.Encrypt(enc_mask, to_ZZX(mask));
-  vector<string> tmp;
-  vector<int> meds;
-  boost::algorithm::split(tmp, argv[5], boost::is_any_of(","));
-  for (string s : tmp) {
-    int medID = stoi(s);
-    assert(medID<totMed);
-    if (find(meds.begin(),meds.end(),medID)==meds.end()) meds.push_back(medID);
-  }
-  assert(meds.size()>0);
-  tmp.clear();
-  vector<int> sides;
-  boost::algorithm::split(tmp, argv[6], boost::is_any_of(","));
-  for (string s : tmp) {
-    int sideID = stoi(s);
-    assert(sideID<totSide);
-    if (find(sides.begin(),sides.end(),sideID)==sides.end()) sides.push_back(sideID);
-  }
-  assert(sides.size()>0);
-  auto start_timer = std::chrono::system_clock::now();
+    cout << return_current_time_and_date()
+         << " Successfully created connection with " << ip_address << ":"
+         << port << endl;
+    int age = stoi(argv[3]);
+    assert(age >= 0 && age <= 122);
+    char gender = *argv[4];
+    assert(gender == 'm' || gender == 'f');
+    int mask = age + (gender == 'm' ? 1 : 0) * 128 + 5;
+    Ctxt enc_mask(publicKey);
+    publicKey.Encrypt(enc_mask, to_ZZX(mask));
+    vector<string> tmp;
+    vector<int> meds;
+    boost::algorithm::split(tmp, argv[5], boost::is_any_of(","));
+    for (string s : tmp)
+    {
+        int medID = stoi(s);
+        assert(medID < totMed);
+        if (find(meds.begin(), meds.end(), medID) == meds.end())
+            meds.push_back(medID);
+    }
+    assert(meds.size() > 0);
+    tmp.clear();
+    vector<int> sides;
+    boost::algorithm::split(tmp, argv[6], boost::is_any_of(","));
+    for (string s : tmp)
+    {
+        int sideID = stoi(s);
+        assert(sideID < totSide);
+        if (find(sides.begin(), sides.end(), sideID) == sides.end())
+            sides.push_back(sideID);
+    }
+    assert(sides.size() > 0);
+    auto start_timer = std::chrono::system_clock::now();
 
-  connect<<enc_mask;
-  connect.flush();
+    connect << enc_mask;
+    connect.flush();
 
-  connect<<meds.size()<<endl;
-  for (int i=0;i<meds.size();++i) connect<<meds[i]<<endl;
+    connect << meds.size() << endl;
+    for (int i = 0; i < meds.size(); ++i)
+        connect << meds[i] << endl;
 
-  connect<<sides.size()<<endl;
-  for (int i=0;i<sides.size();++i) connect<<sides[i]<<endl;
+    connect << sides.size() << endl;
+    for (int i = 0; i < sides.size(); ++i)
+        connect << sides[i] << endl;
 
-  connect.flush();
+    connect.flush();
 
-  int numChunks;
-  connect>>numChunks;
-  vector<pair<int,int>> ret;
-  vector<Ctxt> chunks_res;
-  for (int i=0;i<numChunks;++i){
-    Ctxt chunk_res(publicKey);
-    connect>>chunk_res;
-    chunks_res.push_back(chunk_res);
-  }
-  auto dec_timer = std::chrono::system_clock::now();
-  for (int i=0;i<numChunks;++i){
-    vector<long> decrypted;
-    ea.decrypt(chunks_res[i], secretKey, decrypted);
-    /*
-    cout<<"Chunk #"<<i<<endl;
-    for (int j=0;j<100;++j) cout<<i*100+j<<":"<<decrypted[j]<<" ";
-    cout<<endl;
-    */
-    for (int j=0;j<decrypted.size();++j)
-      if (decrypted[j]==0) ret.push_back(make_pair(i,j));
-  }
-  auto dec_end_timer = std::chrono::system_clock::now();
-  std::chrono::duration<double> diff_dec = dec_end_timer - dec_timer;
+    int numChunks;
+    connect >> numChunks;
+    vector<pair<int, int>> ret;
+    vector<Ctxt> chunks_res;
+    for (int i = 0; i < numChunks; ++i)
+    {
+        Ctxt chunk_res(publicKey);
+        connect >> chunk_res;
+        chunks_res.push_back(chunk_res);
+    }
+    auto dec_timer = std::chrono::system_clock::now();
+    for (int i = 0; i < numChunks; ++i)
+    {
+        vector<long> decrypted;
+        ea.decrypt(chunks_res[i], secretKey, decrypted);
+        /*
+        cout<<"Chunk #"<<i<<endl;
+        for (int j=0;j<100;++j) cout<<i*100+j<<":"<<decrypted[j]<<" ";
+        cout<<endl;
+        */
+        for (int j = 0; j < decrypted.size(); ++j)
+            if (decrypted[j] == 0)
+                ret.push_back(make_pair(i, j));
+    }
+    auto dec_end_timer = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff_dec = dec_end_timer - dec_timer;
 
-  connect<<ret.size()<<endl;
-  for (pair<int,int> ret_item: ret){
-    connect<<ret_item.first<<endl;
-    connect<<ret_item.second<<endl;
-  }
+    connect << ret.size() << endl;
+    for (pair<int, int> ret_item : ret)
+    {
+        connect << ret_item.first << endl;
+        connect << ret_item.second << endl;
+    }
 
-  connect.flush();
+    connect.flush();
 
-  int numRes;
-  connect>>numRes;
-  vector<int> records;
-  for (int i=0;i<numRes;++i){
-    int recID;
-    connect>>recID;
-    records.push_back(recID);
-  }
-  double exec_time, comm_time, percentage;
-  connect>>exec_time>>comm_time>>percentage;
-  auto end_timer = std::chrono::system_clock::now();
-  std::chrono::duration<double> diff = end_timer-start_timer;
-  cout<<"Server has completed running in "<<exec_time<<" seconds."<<endl;
-  cout<<"Communication spent "<<comm_time<<" seconds from server's POV and decryption spent "<<diff_dec.count()<<" seconds from client's POV."<<endl;
-  cout<<"Total record number hit: "<<numRes<<endl;
-  cout<<"The whole session has used "<<diff.count()<<" seconds."<<endl;
-  cout<<return_current_time_and_date()<<" Session with server is over."<<endl;
+    int numRes;
+    connect >> numRes;
+    vector<int> records;
+    for (int i = 0; i < numRes; ++i)
+    {
+        int recID;
+        connect >> recID;
+        records.push_back(recID);
+    }
+    double exec_time, comm_time, percentage;
+    connect >> exec_time >> comm_time >> percentage;
+    auto end_timer = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end_timer - start_timer;
+    cout << "Server has completed running in " << exec_time << " seconds."
+         << endl;
+    cout << "Communication spent " << comm_time
+         << " seconds from server's POV and decryption spent "
+         << diff_dec.count() << " seconds from client's POV." << endl;
+    cout << "Total record number hit: " << numRes << endl;
+    cout << "The whole session has used " << diff.count() << " seconds."
+         << endl;
+    cout << return_current_time_and_date() << " Session with server is over."
+         << endl;
 
-  cout<<"Show result list?(y/n)";
-  char c;
-  cin>>c;
-  if (c=='Y'||c=='y'){
-    cout<<"Result list:"<<endl;
-    for (int i=0;i<numRes;++i) cout<<records[i]<<endl;
-  }
+    cout << "Show result list?(y/n)";
+    char c;
+    cin >> c;
+    if (c == 'Y' || c == 'y')
+    {
+        cout << "Result list:" << endl;
+        for (int i = 0; i < numRes; ++i)
+            cout << records[i] << endl;
+    }
 
-  return 0;
+    return 0;
 }
--- src/update.cpp
+++ src/update.cpp
@@ -1,146 +1,167 @@
-#include<iostream>
-#include<fstream>
-#include<vector>
-#include<algorithm>
-#include<string>
-#include<cassert>
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 // This file update invertedindex
-// Usage: update [FILENAME FOR ENCRYPTED MASK] [num(MedID)] [List<MedID>] [num(SideID)] [List<SideID>]
+// Usage: update [FILENAME FOR ENCRYPTED MASK] [num(MedID)] [List<MedID>]
+// [num(SideID)] [List<SideID>]
 
-int main (int argc, char *argv[]){
+int main(int argc, char* argv[])
+{
 
-  assert(argc>5);
-  cout<<"argc: "<<argc<<endl;
-  // Must contains at least 5 parameters
+    assert(argc > 5);
+    cout << "argc: " << argc << endl;
+    // Must contains at least 5 parameters
 
-  ifstream fdbbasics("../settings/dbbasics.bin", std::ios::binary);
-  bool dbstatus;
-  assert(fdbbasics>>dbstatus);
-  assert(dbstatus);
-  int numRec, totMed, totSide;
-  assert(fdbbasics>>numRec);
-  assert(fdbbasics>>totMed);
-  assert(fdbbasics>>totSide);
-  fdbbasics.close();
+    ifstream fdbbasics("../settings/dbbasics.bin", std::ios::binary);
+    bool dbstatus;
+    assert(fdbbasics >> dbstatus);
+    assert(dbstatus);
+    int numRec, totMed, totSide;
+    assert(fdbbasics >> numRec);
+    assert(fdbbasics >> totMed);
+    assert(fdbbasics >> totSide);
+    fdbbasics.close();
 
-  string filename(argv[1]);
-  try{
-    ifstream ftest(filename.c_str(), std::ios::binary);
-    ftest.close();
-  }
-  catch(const exception& e){
-    cerr<<"Seems problems within the filename or the file itself."<<endl;
-    cerr<<"DB not updated."<<endl;
-    return 1;
-  };
-
-  const string numMed_str(argv[2]);
-  const int numMed=stoi(numMed_str);
-  cout<<"numMed: "<<numMed<<endl;
-
-  vector<int> meds;
-  assert(argc>=5+numMed);
-  for (int i=0;i<numMed;++i){
-    const string medID_str(argv[3+i]);
-    const int medID=stoi(medID_str);
-    assert(medID<totMed);
-    if (find(meds.begin(),meds.end(),medID)==meds.end()) meds.push_back(medID);
-  }
-
-  const string numSide_str(argv[3+numMed]);
-  const int numSide=stoi(numSide_str);
-  cout<<"numSide: "<<numSide<<endl;
-
-  vector<int> sides;
-  assert(argc==4+numMed+numSide);
-  for(int i=0;i<numSide;++i){
-    const string sideID_str(argv[4+numMed+i]);
-    const int sideID=stoi(sideID_str);
-    assert(sideID<totSide);
-    if (find(sides.begin(),sides.end(),sideID)==sides.end()) sides.push_back(sideID);
-  }
-
-  string copy_str="mv "+filename+" ../encdata/"+to_string(numRec)+".bin";
-  cout<<"Calling: "<<copy_str<<endl;
-  system(copy_str.c_str());
-
-  string auxfilename="../auxdata/"+to_string(numRec)+".bin";
-
-  ofstream faux(auxfilename.c_str(), std::ios::binary);
-  faux<<"Medicine: ["<<meds[0];
-  for (int i=1;i<numMed;++i) faux<<", "<<meds[i];
-  faux<<"]"<<endl;
-  faux<<"Side Effect: ["<<sides[0];
-  for (int i=1;i<numSide;++i) faux<<", "<<sides[i];
-  faux<<"]"<<endl;
-  faux.close();
-
-  ofstream fdbbasicsw("../settings/dbbasics.bin", std::ios::binary);
-  fdbbasicsw<<dbstatus<<endl;
-  fdbbasicsw<<numRec+1<<endl;
-  fdbbasicsw<<totMed<<endl;
-  fdbbasicsw<<totSide<<endl;
-  fdbbasicsw.close();
-
-  vector<vector<int>> medIndex;
-  vector<vector<int>> sideIndex;
-  ifstream findexmed ("../auxdata/med.inv", std::ios::binary);
-  int numMedDict;
-  findexmed>>numMedDict;
-  for (int i=0;i<numMedDict;++i){
-    int numindex;
-    findexmed>>numindex;
-    vector<int> tempindex;
-    while (numindex--){
-      int temprec;
-      findexmed>>temprec;
-      tempindex.push_back(temprec);
+    string filename(argv[1]);
+    try
+    {
+        ifstream ftest(filename.c_str(), std::ios::binary);
+        ftest.close();
     }
-    medIndex.push_back(tempindex);
-  }
-  findexmed.close();
+    catch (const exception& e)
+    {
+        cerr << "Seems problems within the filename or the file itself."
+             << endl;
+        cerr << "DB not updated." << endl;
+        return 1;
+    };
 
-  ifstream findexside ("../auxdata/side.inv", std::ios::binary);
-  int numSideDict;
-  findexside>>numSideDict;
-  for (int i=0;i<numSideDict;++i){
-    int numindex;
-    findexside>>numindex;
-    vector<int> tempindex;
-    while (numindex--){
-      int temprec;
-      findexside>>temprec;
-      tempindex.push_back(temprec);
+    const string numMed_str(argv[2]);
+    const int numMed = stoi(numMed_str);
+    cout << "numMed: " << numMed << endl;
+
+    vector<int> meds;
+    assert(argc >= 5 + numMed);
+    for (int i = 0; i < numMed; ++i)
+    {
+        const string medID_str(argv[3 + i]);
+        const int medID = stoi(medID_str);
+        assert(medID < totMed);
+        if (find(meds.begin(), meds.end(), medID) == meds.end())
+            meds.push_back(medID);
     }
-    sideIndex.push_back(tempindex);
-  }
-  findexside.close();
 
-  for (int i=0;i<numMed;++i) medIndex[meds[i]].push_back(numRec);
-  for (int i=0;i<numSide;++i) sideIndex[sides[i]].push_back(numRec);
-  cout<<"Updated rev index"<<endl;
+    const string numSide_str(argv[3 + numMed]);
+    const int numSide = stoi(numSide_str);
+    cout << "numSide: " << numSide << endl;
 
-  ofstream findexmedw ("../auxdata/med.inv", std::ios::binary);
-  findexmedw<<numMedDict<<endl;
-  for (int i=0;i<numMedDict;++i){
-    findexmedw<<medIndex[i].size()<<endl;
-    for (int j=0;j<medIndex[i].size();++j) findexmedw<<medIndex[i][j]<<endl;
-  }
-  findexmedw.close();
+    vector<int> sides;
+    assert(argc == 4 + numMed + numSide);
+    for (int i = 0; i < numSide; ++i)
+    {
+        const string sideID_str(argv[4 + numMed + i]);
+        const int sideID = stoi(sideID_str);
+        assert(sideID < totSide);
+        if (find(sides.begin(), sides.end(), sideID) == sides.end())
+            sides.push_back(sideID);
+    }
 
-  ofstream findexsidew ("../auxdata/side.inv", std::ios::binary);
-  findexsidew<<numSideDict<<endl;
-  for (int i=0;i<numSideDict;++i){
-    findexsidew<<sideIndex[i].size()<<endl;
-    for (int j=0;j<sideIndex[i].size();++j) findexsidew<<sideIndex[i][j]<<endl;
-  }
-  findexsidew.close();
+    string copy_str =
+      "mv " + filename + " ../encdata/" + to_string(numRec) + ".bin";
+    cout << "Calling: " << copy_str << endl;
+    system(copy_str.c_str());
 
-  cout<<"New record as: #"<<numRec<<endl;
-  cout<<"DB successfully updated."<<endl;
+    string auxfilename = "../auxdata/" + to_string(numRec) + ".bin";
 
-  return 0;
+    ofstream faux(auxfilename.c_str(), std::ios::binary);
+    faux << "Medicine: [" << meds[0];
+    for (int i = 1; i < numMed; ++i)
+        faux << ", " << meds[i];
+    faux << "]" << endl;
+    faux << "Side Effect: [" << sides[0];
+    for (int i = 1; i < numSide; ++i)
+        faux << ", " << sides[i];
+    faux << "]" << endl;
+    faux.close();
 
+    ofstream fdbbasicsw("../settings/dbbasics.bin", std::ios::binary);
+    fdbbasicsw << dbstatus << endl;
+    fdbbasicsw << numRec + 1 << endl;
+    fdbbasicsw << totMed << endl;
+    fdbbasicsw << totSide << endl;
+    fdbbasicsw.close();
+
+    vector<vector<int>> medIndex;
+    vector<vector<int>> sideIndex;
+    ifstream findexmed("../auxdata/med.inv", std::ios::binary);
+    int numMedDict;
+    findexmed >> numMedDict;
+    for (int i = 0; i < numMedDict; ++i)
+    {
+        int numindex;
+        findexmed >> numindex;
+        vector<int> tempindex;
+        while (numindex--)
+        {
+            int temprec;
+            findexmed >> temprec;
+            tempindex.push_back(temprec);
+        }
+        medIndex.push_back(tempindex);
+    }
+    findexmed.close();
+
+    ifstream findexside("../auxdata/side.inv", std::ios::binary);
+    int numSideDict;
+    findexside >> numSideDict;
+    for (int i = 0; i < numSideDict; ++i)
+    {
+        int numindex;
+        findexside >> numindex;
+        vector<int> tempindex;
+        while (numindex--)
+        {
+            int temprec;
+            findexside >> temprec;
+            tempindex.push_back(temprec);
+        }
+        sideIndex.push_back(tempindex);
+    }
+    findexside.close();
+
+    for (int i = 0; i < numMed; ++i)
+        medIndex[meds[i]].push_back(numRec);
+    for (int i = 0; i < numSide; ++i)
+        sideIndex[sides[i]].push_back(numRec);
+    cout << "Updated rev index" << endl;
+
+    ofstream findexmedw("../auxdata/med.inv", std::ios::binary);
+    findexmedw << numMedDict << endl;
+    for (int i = 0; i < numMedDict; ++i)
+    {
+        findexmedw << medIndex[i].size() << endl;
+        for (int j = 0; j < medIndex[i].size(); ++j)
+            findexmedw << medIndex[i][j] << endl;
+    }
+    findexmedw.close();
+
+    ofstream findexsidew("../auxdata/side.inv", std::ios::binary);
+    findexsidew << numSideDict << endl;
+    for (int i = 0; i < numSideDict; ++i)
+    {
+        findexsidew << sideIndex[i].size() << endl;
+        for (int j = 0; j < sideIndex[i].size(); ++j)
+            findexsidew << sideIndex[i][j] << endl;
+    }
+    findexsidew.close();
+
+    cout << "New record as: #" << numRec << endl;
+    cout << "DB successfully updated." << endl;
+
+    return 0;
 }
