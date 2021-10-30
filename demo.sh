9735c5bc8163bcfb727dbeca7bb1569558856645
Kotaro Inoue <kinoue@yama.info.waseda.ac.jp>
2021-10-30 17:06:32 +0900

    Fix typo

--- demo.sh
+++ demo.sh
@@ -3,7 +3,7 @@
 TOPDIR=`pwd`
 BINDIR=${TOPDIR}/build/demo
 CONTEXTFILE=${TOPDIR}/settings/contextsetting.txt
-CSVFILE=${TOPDIR}/dmydata/demo.csv
+CSVFILE=${TOPDIR}/dmydata/data.csv
 
 xterm -T "Client" -e "/bin/bash -c 'cd ${BINDIR}/client  && ./client -c ${CONTEXTFILE} 20 m 2815 146; exec /bin/bash -i'"&
 xterm -T "Server" -e "/bin/bash -c 'cd ${BINDIR}/server  && ./server -f ${CSVFILE};                   exec /bin/bash -i'"&
